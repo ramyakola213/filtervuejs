7168cb63bc1d878a38f835a4299b37d6ea2e0bd4
ramyakola213 <ramya.kola@zensar.com>
2022-06-02 11:41:59 +0530

    addfilter

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+   <div class="container">
+      <h1> Vuejs Filter Search </h1>
+      <form v-on:submit="resetForm">
+      <input type="text" name="searchbar" role="seacrh" v-model="input" placeholder="Search from here" /><br>
+       <button role="button" type="submit" bname="Reset" >Reset</button>
+      </form>
+      <div class="item tech" v-for="tech in filteredList()" :key="tech">
+         <p>{{ tech }}</p>
+      </div>
+      <div class="item error" v-if="input&&!filteredList().length">
+         <p>No results..</p>
+      </div>
+    </div>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+   import { ref } from "vue";
+      let input = ref("");
+      const technlogies = ["Javascript", "Angular", "Vuejs", "Nodejs","Reactjs","Java","Dotnet"];
+      function filteredList() {
+          return technlogies.filter((tech) =>
+           tech.toLowerCase().includes(input.value.toLowerCase())
+          );
+       }
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container
+{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10% ;
+}
+h1{
+  color: green;
+}
+input{
+  border: 2px solid green;
+  padding: 10px 20px;
+  margin: 10px;
+}
+input[type=text]:focus{
+  
+  outline: none;
+
 }
 </style>
+
+
---
+++ src/components/Post-comp.vue
@@ -0,0 +1,12 @@
+ <!--<template>
+        <article>
+                <h3> {{ Post.title }}</h3>
+        </article>
+</template>
+
+<script>
+export default {
+    props :["Post"]    
+};
+</script>
+-->
